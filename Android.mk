6bf86356904a5de1be3c62fa2011d0141ca1f4d7
xinliang.liu <xinliang.liu@linaro.org>
2015-06-08 12:02:41 +0800

    Change to LOCAL_MODULE_RELATIVE_PATH
    Signed-off-by: xinliang.liu <xinliang.liu@linaro.org>

--- Android.mk
+++ Android.mk
@@ -19,15 +19,15 @@ LOCAL_PATH := $(call my-dir)
 # hw/<OVERLAY_HARDWARE_MODULE_ID>.<ro.product.board>.so
 include $(CLEAR_VARS)
 
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SHARED_LIBRARIES := liblog libdrm libkms libhardware libsync
 #libion
 LOCAL_SRC_FILES := hwcomposer.cpp
-LOCAL_MODULE := hwcomposer.default
+LOCAL_MODULE := hwcomposer.$(TARGET_BOARD_PLATFORM)
 LOCAL_CFLAGS:= -DLOG_TAG=\"hwcomposer\"
 LOCAL_MODULE_TAGS := eng
 LOCAL_C_INCLUDES += \
-        $(TOP)/hardware/libhardware/modules/gralloc \
+        $(TOP)/hardware/hisi/display/gralloc-drm \
         $(TOP)/external/drm \
         $(TOP)/external/drm/include/drm \
         $(TOP)/hardware/libhardware/include \
